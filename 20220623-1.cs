6d77dd66c600da476d7501bf515ae8762d21be56
ste-s <103716982+ste-s@users.noreply.github.com>
2022-06-28 14:06:14 -0300

    Add files via upload

---
+++ 20220623-1.cs
@@ -0,0 +1,30 @@
+﻿//Ler um número e informar se ele é divisível por 3 e por 7.
+
+int num;
+
+Console.WriteLine("Insira um número qualquer: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if ((0 == (num % 3)) && (0 == (num % 7)))
+{
+    Console.WriteLine(num + " é divisível por 3 e 7.");
+}
+else
+{
+    if ((0 != (num % 3)) && (0 == (num % 7)))
+    {
+        Console.WriteLine(num + " é divisível por 7 mas não é por 3.");
+    }
+    else
+    {
+        if ((0 == (num % 3)) && (0 != (num % 7)))
+        {
+            Console.WriteLine(num + " é divisível por 3 mas não é por 7.");
+        }
+        else 
+        {
+            Console.WriteLine(num + "  não é divisível por 3 nem por 7");        
+        }
+    }
+
+}
